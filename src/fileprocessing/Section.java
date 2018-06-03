e658e5ccd89d24c40c72fa236af71220cede0a4e
gilgil999 <gil.ronen.39.talpiot@gmail.com>
2018-06-03 23:50:05 +0300

    n2

--- src/fileprocessing/Section.java
+++ src/fileprocessing/Section.java
@@ -1,4 +1,16 @@
 package fileprocessing;
+import
+
+import java.io.File;
+import java.util.Comparator;
 
 public class Section {
+    private Filterable filter;
+    private Comparator<File> order;
+
+    public Section(Filterable filter, Comparator<File> order) {
+        this.filter = filter;
+        this.order = order;
+    }
+
 }
