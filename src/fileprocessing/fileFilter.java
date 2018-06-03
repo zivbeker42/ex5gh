6451f633d2b3751df1015e6c06af6a4aeb8c742f
ZivBeker <ziv.beker@gmail.com>
2018-06-03 20:35:30 +0300

    a good start

---
+++ src/fileprocessing/fileFilter.java
@@ -0,0 +1,15 @@
+package fileprocessing;
+
+public class fileFilter {
+
+    /*The file's name that passes this filter*/
+    private String name;
+
+    /**
+     * Creates a new file filter with a given file's name
+     * @param name
+     */
+    public fileFilter(String name) {
+        this.name = name;
+    }
+}
