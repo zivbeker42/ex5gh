0d37c261a04a5a088c38129edd366e5ff9ba957d
gilgil999 <gil.ronen.39.talpiot@gmail.com>
2018-06-04 21:28:00 +0300

    new filter hirarcy

--- src/fileprocessing/filters/permissionFilter.java
+++ src/fileprocessing/filters/permissionFilter.java
@@ -8,43 +8,35 @@ public class permissionFilter implements Filterable {
     //private static String[] allowedTypes = {"WRITABLE", "EXECUTABLE", "hidden"};
 
     /*The type of the filter. can be either "WRITABLE", "EXECUTABLE", "hidden".*/
-    private filterType type;
-
+    private Filterable.filterType type;
     /*The boolean factor that says whether .*/
     private boolean value;
 
     /**
      * A constructor for greater than and smaller than filters.
-     * @param type the type of the filter.
+     *
+     * @param type  the type of the filter.
      * @param value the value of the filter.
      * @throws Exception
      */
-    public permissionFilter(String type, String value) throws Exception{
-        this.type = new filterType(type);
-        if (!this.type.permissionFilter) {
-            throw new Exception("not a permission type.");
-        }
+    public permissionFilter(Filterable.filterType type, String value) {
+        this.type = type;
         if (value.equals("YES")) {
             this.value = true;
-        }
-        else if (value.equals("NO")) {
+        } else if (value.equals("NO")) {
             this.value = false;
         }
-
-
-
     }
 
     @Override
-    public boolean filter(File file){
-        if(type.name == filterType.filterName.EXECUTABLE) {
+    public boolean filter(File file) {
+        if (this.type == Filterable.filterType.EXECUTABLE) {
             return file.canExecute() == value;
-
         }
-        if(type.name == filterType.filterName.WRITABLE) {
+        if (this.type == Filterable.filterType.WRITABLE) {
             return file.canWrite() == value;
         }
-        if(type.name == filterType.filterName.HIDDEN) {
+        if (this.type == Filterable.filterType.HIDDEN) {
             return file.isHidden() == value;
         }
         return false;
--- src/fileprocessing/filters/stringFilters.java
+++ src/fileprocessing/filters/stringFilters.java
@@ -7,10 +7,10 @@ import java.util.Arrays;
  * A class implementing a filter that is described by a string checking the file's name. Meaning it is
  * responsible for file, contains, prefix, suffix filter.
  */
-public class stringFilters implements Filterable{
+public class stringFilters implements Filterable {
 
     /*The type of the filter.*/
-    private filterType type;
+    private Filterable.filterType type;
 
     /*The string that passes the filter (depending on it's type) .*/
     private String value;
@@ -18,33 +18,30 @@ public class stringFilters implements Filterable{
     /**
      * A constructor for a string filter by a given value that describes the filter and a string describing
      * it's type.
-     * @param type the type of the filter.
+     *
+     * @param type  the type of the filter.
      * @param value the value of the filter.
      * @throws Exception iff type is not a string filter.
      */
-    public stringFilters(String type, String value) throws Exception{
-        this.type = new filterType(type);
+    public stringFilters(Filterable.filterType type, String value) {
+        this.type = type;
         this.value = value;
-        if(!this.type.stringFilter){
-            throw new Exception("not a string type");
-        }
-
     }
 
 
     @Override
-    public boolean filter(File file){
-        if (type.name == filterType.filterName.FILE){
+    public boolean filter(File file) {
+        if (type == Filterable.filterType.FILE) {
             return file.getName().equals(value);
-        }
-        else if(type.name == filterType.filterName.CONTAINS){
+        } else if (type == Filterable.filterType.CONTAINS) {
             return file.getName().contains(value);
-        }
-        else if(type.name == filterType.filterName.SUFFIX){
+        } else if (type == Filterable.filterType.SUFFIX) {
             return file.getName().endsWith(value);
-        }
-        else {
+        } else if (type == Filterable.filterType.PREFIX) {
             return file.getName().startsWith(value);
+        } else {
+            //unsupported
+            return false;
         }
 
     }
