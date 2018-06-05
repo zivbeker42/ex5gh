c10bd469b8b28faee6d10759b8015f8decce002f
gilgil999 <gil.ronen.39.talpiot@gmail.com>
2018-06-05 21:23:26 +0300

    exeption handling in the parse method, further advancements in the parse method

--- src/filesprocessing/SectionFactory.java
+++ src/filesprocessing/SectionFactory.java
@@ -53,6 +53,7 @@ public class SectionFactory {
         //order content
         Filterable filter = null;
         Comparator<File> order;
+        LinkedList<Type1Exception> errors = new LinkedList<Type1Exception>();
         for (String line : rawtext) {
             int linenum = part % LINESPERSECTION; //this variable saves the part is the section that is being currently
             //obsereved
@@ -66,16 +67,16 @@ public class SectionFactory {
                     throw new OrderError();
                 }
                 try {
-                    filter = interpretFilter(line);
-                } catch (IOException e) {
-                    System.err.println("Warning in line " + linecount);
+                    filter = interpretFilter(line,linecount);
+                } catch (Type1Exception error) {
+                    errors.add(error);
                     filter = getDeafaultFilter();
                 }
             } else if (linenum == 3) {
                 validateOrder(line);
                 if(linecount==len){
                     order=getDeafaultComparator();
-                    lst.add(new Section(filter, order));
+                    lst.add(new Section(filter, order, errors));
                 }
             } else if (linenum == 0) {
                 if (line.equals(BUFFERSTRING)) {
@@ -85,13 +86,13 @@ public class SectionFactory {
                     part++;
                 }else{
                     try {
-                        order = interpretOrder(line);
-                    } catch (IOException error) {
-                        System.err.println("Warning in line " + linecount);
+                        order = interpretOrder(line, linecount);
+                    } catch (Type1Exception error) {
+                        errors.add(error);
                         order = getDeafaultComparator();
                     }
                 }
-                lst.add(new Section(filter, order));
+                lst.add(new Section(filter, order, errors));
             } else {
                 //if in the future more segemets will be present in each section
             }
@@ -104,11 +105,11 @@ public class SectionFactory {
 
     }
 
-    private static Comparator<File> interpretOrder(String line) throws IOException {
+    private static Comparator<File> interpretOrder(String line, int linenum) throws Type1Exception {
         if (line.matches(".*#"+REVERSE)) {
             int j = line.indexOf("#"+REVERSE);
             String newline = line.substring(0, j);
-            return interpretOrder(newline).reversed();
+            return interpretOrder(newline, linenum).reversed();
         } else {
             if (line.matches("abs")) {
                 return Orders.absComparator();
@@ -118,7 +119,7 @@ public class SectionFactory {
                 return Orders.sizeComparator();
             }
         }
-        throw new IOException();
+        throw new Type1Exception(linenum);
     }
 
     private static void validateOrder(String line) throws OrderError {
@@ -129,11 +130,11 @@ public class SectionFactory {
 
     }
 
-    private static Filterable interpretFilter(String line) throws IOException {
+    private static Filterable interpretFilter(String line , int i) throws Type1Exception {
         if (line.matches(".*#"+NOT)) {
             int j = line.indexOf("#"+NOT);
             String newline = line.substring(0, j);
-            return new negateFilter(interpretFilter(newline));
+            return new negateFilter(interpretFilter(newline,i));
         } else {
 
             if (line.matches("greater_than#((\\d)+(\\.\\d+)?)")) {
@@ -185,7 +186,7 @@ public class SectionFactory {
             } else if (line.matches("all")) {
                 return new allFilter();
             }
-            throw new IOException();
+            throw new Type1Exception(i);
 
         }
 
