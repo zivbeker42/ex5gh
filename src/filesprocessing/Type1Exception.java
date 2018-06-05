87d89cf9c2d0c3ab21f9f1dd29341f14fab85352
ZivBeker <ziv.beker@gmail.com>
2018-06-05 21:18:05 +0300

    Type1Exception

--- src/filesprocessing/Type1Exception.java
+++ src/filesprocessing/Type1Exception.java
@@ -1,19 +1,26 @@
 package filesprocessing;
 
+/**
+ * A class representing a type1 exception.
+ */
 public class Type1Exception extends Exception{
 
+    /*The line in which the exception occurred.*/
     private int line;
+    /*The message the exception prints.*/
     private static final String message = "Warning in line ";
 
     /**
-     *
-     * @param line
+     * Creates a new Type1Exception.
+     * @param line the line in which the exception occurred.
      */
     public Type1Exception(int line) {
         this.line = line;
     }
 
-
+    /**
+     * prints the exception.
+     */
     public void print() {
         System.err.print(message+line);
     }
